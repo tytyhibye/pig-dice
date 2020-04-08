bf7ec3b4f48e09ca84225eb5e0759cf1b9958d78
Tyler Bates <Fondlytyler@gmail.com>
2020-04-08 11:14:21 -0700

    add roll algorithm to return console log of output and add additional css formatting for placeholders of returns.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,52 @@
 // Back-end logic //
-function randomDice(dice) {
-  var roll = Math.floor(Math.random(dice)*6 + 1);
-  console.log(roll);
+function Game() {
+  this.playersScore = 0;
+  this.compScore = 0;
 }
-thisRoll();
+
+Game.prototype.addToScore = function(roll) {
+  currentRoll = this.roll();
+  console.log(currentRoll);
+  
+  if (roll != 1) {
+    this.playersScore = this.playersScore + roll;
+    console.log(this.playersScore);
+  }
+  // if (roll === 1) {
+  //   this.over =true;
+  //   this.points = 0;
+  // } else {
+  //   this.points += roll;
+  // }
+  // return roll;
+}
+
+
+Game.prototype.roll = function(dice) {
+  let roll = Math.floor(Math.random(dice)*6 + 1);
+  return roll;
+}
+
+let game = new Game();
+game.roll();
+game.addToScore();
+
 // Front-end logic //
 
+// function clickToRoll() {   // roll function
+//   $("#dice").on("click","img", function(){
+//     roll(dice);
+//     console.log(roll)
+//   });
+// }
+
+// game.randomDice()
 $(document).ready(function() {
-  $(".player-name").submit(function(event) {
+  $("form#enter-name").submit(function(event) {
     event.preventDefault();
+    var playerName = $("input#name").val();
+    $("#enter-name").fadeToggle();
+    $("#player-name").text(playerName);
+    $("#computer-name").fadeToggle();
   });
 });
