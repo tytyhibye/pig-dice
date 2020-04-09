c7052184066d19d1c4e2b8a5c87e7374c398a59a
Tyler Bates <Fondlytyler@gmail.com>
2020-04-09 13:47:36 -0700

    exceeded in functionality of player switch prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,55 @@
 // Back-end logic //
 
-function Game(currentPlayer) {
-  this.players = ['player', 'comp'];
+function Game() {
   this.roundScore = 0;
   this.playersScore = 0;
-  this.currentPlayer = currentPlayer;
+  this.compScore = 0;
+  this.currentPlayer = true;
 }
 
 Game.prototype.addToScore = function () {
   currentRoll = this.roll();
-  if (currentRoll != 1 || this.playerSwitch != true) {
+  if (currentRoll != 1) {
     this.roundScore += currentRoll;
     // console.log(this.roundScore);
   } else if (currentRoll === 1) {
+    console.log("rolled a 1");
   }
 };
 
 Game.prototype.switchPlayers = function () {
-  for (var i = 0; i < this.players.length; i++) {
-    if (i === this.players[0]) {
-      currentPlayer = this.players[1];
-      $('#player-score').text('0');
-    } else {
-      currentPlayer = this.players[0];
-      $('#comp-score').text('0');
-    }
+  // 
+  this.currentPlayer = !this.currentPlayer;
+if (currentPlayer === true) {
+  $('#player-score').text('0');
+  currentPlayer = false;
+} else {
+  $('#comp-score').text('0');
+  currentPlayer = true;
   }
   this.roundScore = 0;
-};
+}
+  // }  return this.currentPlayer;
+
+console.log(this.currentPlayer)
+// Game.prototype.switchPlayers2 = function () {
+//   this.playerOneIsUp = !this.playerOneIsUp; // true / false
+// }
+
+// this.currentPlayerIndex = 1 - this.currentPlayerIndex;
+  
+  // this.currentPlayer = ???
+
+  // for (var i = 0; i < this.players.length; i++) {
+  //   if (i === this.players[0]) { // if (0 === 'comp') { // if (1 === 'comp') { 
+  //     currentPlayer = this.players[1];
+  //     $('#comp-score').text('0');
+  //   } else {
+  //     currentPlayer = this.players[1];
+  //     $('#player-score').text('0');
+  //   }
+  // }
+  // this.roundScore = 0;
 //   this.currentPlayer === 1 ? 2 : 1;
 // }
 // this.currentPlayer = !this.currentPlayer
@@ -47,7 +69,7 @@ Game.prototype.clickToRoll = function () {
   var that = this;
   $('#img').click(function () {
     that.addToScore();
-    if (currentPlayer === that.players[0]) {
+    if (currentPlayer === true) {
     $('#player-score').text(that.roundScore);
     } else {
       $('#comp-score').text(that.roundScore);
@@ -60,13 +82,13 @@ Game.prototype.clickToRoll = function () {
 };
 
 $(document).ready(function () {
-  let game = new Game();
+  let game = new Game(); // let game = new Game(0);
   game.clickToRoll();
   $('form#enter-name').submit(function (event) {
     event.preventDefault();
     var playerName = $('input#name').val();
-    currentPlayer = game.players[0];
-    console.log(currentPlayer);
+    currentPlayer = true;
+    // console.log(currentPlayer);
     // console.log(this.players);
     $('#enter-name').fadeOut(200);
     $('#player-name').html("<h3 class='tags'>" + playerName + '</h3>');
@@ -76,5 +98,10 @@ $(document).ready(function () {
     event.preventDefault();
     game.switchPlayers();
     $('#holdButton').fadeOut(1000);
+  //if(currentPlayer===""){
+  //$(player-total).text(currentPlayer.roundScore + playersScore)
+  //} else {
+  //$(comp-total).text(currentPlayer.roundScore + compScore)
+  //}
   });
 });
